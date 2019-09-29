739085308d2c98d7affc7ac9a824b73136cc33c2
Veso <Ambroza8888@gmail.com>
2019-09-29 15:12:23 -0700

    forloop

--- FirstCSharp/Program.cs
+++ FirstCSharp/Program.cs
@@ -68,18 +68,47 @@ namespace FirstCSharp
     //     foreach(string i in bikes){
     //         Console.WriteLine("- " + i);
     //     }    
-Dictionary<string,string> profile = new Dictionary<string,string>();
-//Almost all the methods that exists with Lists are the same with Dictionaries
-profile.Add("Name", "Speros");
-profile.Add("Language", "PHP");
-profile.Add("Location", "Greece");
-// Console.WriteLine("Instructor Profile");
-// Console.WriteLine("Name - " + profile["Name"]);
-// Console.WriteLine("From - " + profile["Location"]);
-// Console.WriteLine("Favorite Language - " + profile["Language"]);
-foreach(KeyValuePair<string,string> veso in profile){
-    Console.WriteLine(veso.Key);
-}
+// Dictionary<string,string> profile = new Dictionary<string,string>();
+// //Almost all the methods that exists with Lists are the same with Dictionaries
+// profile.Add("Name", "Speros");
+// profile.Add("Language", "PHP");
+// profile.Add("Location", "Greece");
+// // Console.WriteLine("Instructor Profile");
+// // Console.WriteLine("Name - " + profile["Name"]);
+// // Console.WriteLine("From - " + profile["Location"]);
+// // Console.WriteLine("Favorite Language - " + profile["Language"]);
+// foreach(KeyValuePair<string,string> veso in profile){
+//     Console.WriteLine(veso.Key);
+// }
+    // Create an array to hold integer values 0 through 9
+    // int[] arr = new int[10];
+    // for (var i = 0; i < arr.Length;i++){
+    //     arr[i]= i;
+    //     Console.WriteLine(i);
+    // }
+    // int[] arr2 = {1,2,3,4,5,6,7,8,9};
+    // foreach( var i in arr2){
+    // Console.WriteLine(i);
+    // }
+    // // Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+    // string[] arr3 = {"Tim","Martin","Nikki","Sara"};
+    // foreach (var i in arr3){
+    //     Console.WriteLine(i);
+    // }
+    // Create an array of length 10 that alternates between true and false values, starting with true
+    bool[] arr4 = new bool[10];
+    for (var i = 0; i < arr4.Length;i++){
+        if(i % 2 == 0){
+            arr4[i] = true;
+        }
+        else{
+            arr4[i] = false;
+        }
+    }
+    foreach (var i in arr4){
+        Console.WriteLine(i);
+    }
+
 
 
 
