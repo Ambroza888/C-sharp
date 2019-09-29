1096579e0afed32d864bd1d61ddec8dbf8f3a6c2
Veso <Ambroza8888@gmail.com>
2019-09-28 18:31:35 -0700

    Hello C#

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string place = "Coding Dojo";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(place);
+        }
+    }
+}
