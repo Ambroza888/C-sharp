eabc0659dae9c8c7a6d866dd86db80bf04d0ed9f
Veso <Ambroza8888@gmail.com>
2019-10-01 14:25:07 -0700

    Finished Human

--- OOP/Human/Program.cs
+++ OOP/Human/Program.cs
@@ -5,13 +5,13 @@ namespace Human
 
     class Human
     {
-        //attributes
-        public string Name;
-        public int Strength;
-        public int Intelligence;
-        public int Dexterity;
-        private int health;
-        // Constructor
+        //attributes all are PRIVATE for that i am using Properties to access them;
+        string Name;
+        int Strength;
+        int Intelligence;
+        int Dexterity;
+        int health;
+        // Constructor===========================================
         public Human(string nam){
             Name = nam;
             Strength = 3;
@@ -27,7 +27,7 @@ namespace Human
             health = hp;
         }
 
-        //Properties
+        //Properties===================================
         public int H{
             get {
                 return health;
@@ -36,27 +36,43 @@ namespace Human
                 health = value;
             }
         }
-        //methods
+        public int Str{
+            get {
+                return Strength;
+            }
+            set {
+                Strength = value;
+            }
+        }
+        public int intProp{
+            get { return Intelligence; }
+            set { Intelligence = value; }
+        }
+
+        //methods =============================================
         public int Attack(Human Target){
             Target.H = Target.H - (5 * Strength);
             Console.WriteLine($"Remaining health of the target is {Target.H}");
             return Target.H;
-
+        }
+        public int StealStr(Human target){
+            target.Str = target.Str - 14;
+            Console.WriteLine($"The  {target.Name} got stolen 14str from {this.Name}");
+            return target.Str;
+        }
+        public int Healing(Human target){
+            return target.H= target.H + (8*Intelligence);
         }
     }
-
-
-
-    
     class Program
     {
         static void Main(string[] args)
         {
-
-            Human veso = new Human("veso");
-            Human stas = new Human("Stas");
-            veso.Attack(stas);
+            Human veso = new Human("veso", 50,50,50,100);
+            Human stas = new Human("Stas",50,50,50,100);
+            veso.Healing(stas);
             Console.WriteLine(stas.H);
+            
 
 
 
