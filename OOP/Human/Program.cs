f9095a3e8584db50606ecada3d908a9307654613
Veso <Ambroza8888@gmail.com>
2019-09-30 21:20:15 -0700

    Properties

---
+++ OOP/Human/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Human
+{
+
+
+
+
+
+    class Vehicle
+    {
+        int maxNumPassengers;
+        string color;
+        public Vehicle(int maxPass, string col){
+            maxNumPassengers = maxPass;
+            color = col;
+        }
+        public int MaxNumPassengers
+        {
+            get { return maxNumPassengers; }
+        }
+        public string Color
+        {
+            get { return color; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Vehicle veso = new Vehicle(10,"Green");
+            Console.WriteLine(veso.Color);
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
