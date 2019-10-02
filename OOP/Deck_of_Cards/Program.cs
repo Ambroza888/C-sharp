41f9978646096f596525ea5b1c923da4e89b28b4
Veso <Ambroza8888@gmail.com>
2019-10-01 21:56:52 -0700

    Shuffle

--- OOP/Deck_of_Cards/Program.cs
+++ OOP/Deck_of_Cards/Program.cs
@@ -23,11 +23,6 @@ namespace Deck_of_Cards
         public string[] strVal = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
         public string[] suit = {"Clubs","Spades","Hearths","Diamonds"};
         public int[] val = {1,2,3,4,5,6,7,8,9,10,11,12,13};
-
-
-
-
-
         //========= Constructor========
         public Deck()
         {
@@ -39,23 +34,42 @@ namespace Deck_of_Cards
             cards = new List<Card>();
             for (int i =0 ; i < suit.Length;i++){
                 for (int j=0 ; j < val.Length;j++){
-                    cards.Add(new Card(strVal[j],suit[i],val[j]));
+                    Card nCard = new Card(strVal[j],suit[i],val[j]);
+                    // Console.WriteLine($"the Value is :{nCard.val} THE STRVALUE :{nCard.stringVal} suit is : {nCard.suit}");
+                    cards.Add(nCard);
                 }
             }
-            System.Console.WriteLine("DECK IS MADE!!!");
         }
         public Card PopTopCard()
         {
             Card c = cards[cards.Count-1];
+            // Console.WriteLine(c.stringVal);
+            // Console.WriteLine(c.suit);
+            // Console.WriteLine(c.val);
             cards.RemoveAt(cards.Count-1);
             return c;
         }
-
         public void ResetDeck()
         {
             cards.Clear();
             GenerateDeck();
         }
+        public void Shuffle()
+        {
+                Random rand = new Random();
+            int num1;
+            int num2;
+            Card temp;
+            for (var i = 0 ; i < 100;i++){
+
+                num1 = rand.Next(0,cards.Count);
+                num2 = rand.Next(0,cards.Count);
+                temp = cards[num1];
+                cards[num1] = cards[num2];
+                cards[num2] = temp;
+            }
+        }
+
 
 
 
@@ -74,10 +88,11 @@ namespace Deck_of_Cards
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!");
             Deck d = new Deck();
-            Console.WriteLine($"The val is : {d.PopTopCard().val} and suit is {d.PopTopCard().suit} and {d.PopTopCard().stringVal}");
-
+            Card t = d.PopTopCard();
+            d.Shuffle();
+            d.ResetDeck();
         }
     }
 }
