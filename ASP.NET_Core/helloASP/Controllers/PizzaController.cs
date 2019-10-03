ed1fb84bf88cb3b82864456452aad5a9411b08db
Veso <Ambroza8888@gmail.com>
2019-10-03 16:50:18 -0700

    Portfolio

--- ASP.NET_Core/helloASP/Controllers/PizzaController.cs
+++ ASP.NET_Core/helloASP/Controllers/PizzaController.cs
@@ -3,23 +3,44 @@ namespace helloASP.Controllers
 {
   public class PizzaController : Controller
   {
-    //home
+    //Portfolio assigmment
     [HttpGet("")]
     public string Pizza()
     {
       return "This is my Pizza";
     }
-    ///Pizza
+
+    //Portfolio assigments II
     [HttpGet("{projects}")]
     public string hello(string projects)
     {
       return $"Hello From {projects}";
     }
-    // id passing params localhost:5000/users/???
+
+    // Portfolio Assigments III
     [HttpGet("users/{username}/{papi}")]
     public string HelloUser(string username,string papi)
     {
       return $"Hello {username} and you live in {papi}"; 
+    }
+    // ================= Views introductions ===============
+    
+    [HttpGet("vvv")]
+    public ViewResult index()
+    {
+      return View("index");
+    }
+    // Redirect from vvv --- go ---- > bra
+    [HttpGet("go")]
+    public RedirectResult go()
+    {
+      return Redirect("bra");
+    }
+    // render View("BRA")
+    [HttpGet("bra")]
+    public ViewResult bra()
+    {
+      return View("bra");
     }
 
 
