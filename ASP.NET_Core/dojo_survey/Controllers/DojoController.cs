dbf5e9affc1956bf23fa2550ed13611710077e6b
Veso <Ambroza8888@gmail.com>
2019-10-04 11:04:24 -0700

    done'

--- ASP.NET_Core/dojo_survey/Controllers/DojoController.cs
+++ ASP.NET_Core/dojo_survey/Controllers/DojoController.cs
@@ -4,14 +4,26 @@ namespace dojo_survey.Controllers
   public class DojoController : Controller
   {
 
-    // ==== Home =====
+    // -------------------------------------------------------------------------
+    // Home http
+    // -------------------------------------------------------------------------
     [HttpGet("")]
     public IActionResult Home()
     {
       return View("index");
     }
-
-
+  // ---------------------------------------------------------------------------
+  // result hhtp
+  // ---------------------------------------------------------------------------
+    [HttpPost("result")]
+    public IActionResult Reeqwweqsult(string name, string location,string lang, string commend)
+    {
+      ViewBag.name = name;
+      ViewBag.location = location;
+      ViewBag.lang = lang;
+      ViewBag.commend = commend;
+      return View("result");
+    }
 
   }
 }
