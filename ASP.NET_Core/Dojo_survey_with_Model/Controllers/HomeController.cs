81d4f2f0d14ec52ee21bab4cdb0390eb89fd74ab
Veso <Ambroza8888@gmail.com>
2019-10-04 17:39:00 -0700

    Done

---
+++ ASP.NET_Core/Dojo_survey_with_Model/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Dojo_survey_with_Model.Models;
+
+namespace Dojo_survey_with_Model.Controllers
+{
+    public class HomeController : Controller
+    {
+        // ---------------------------------------------------------------------
+        // Home Page
+        // ---------------------------------------------------------------------
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+
+
+// -----------------------------------------------------------------------------
+// Survey HTTP
+// -----------------------------------------------------------------------------
+        [HttpPost("survey")]
+        public IActionResult Submission(Survey yo)
+        {
+
+            return View("result",yo);
+        }
+
+
+
+
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
